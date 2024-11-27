db4c224bdf6338edf702097748bf2008a3f4fc7e
ZOUBAIRDouae <zoubairdouae5@gmail.com>
2024-11-27 12:45:37 +0000

    module3

---
+++ Module-3/tuto7/app/src/main/java/com/example/tuto71/tuto7.1.kt
@@ -0,0 +1,141 @@
+package com.example.tuto71
+
+
+/*class FillInTheBlankQuestion(
+    val questionText: String,
+    val answer: String,
+    val difficulty: String
+)
+
+class TrueOrFalseQuestion(
+    val questionText: String,
+    val answer: Boolean,
+    val difficulty: String
+)
+
+class NumericQuestion(
+    val questionText: String,
+    val answer: Int,
+    val difficulty: String
+)*/
+
+
+data class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: Difficulty
+)
+
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
+
+
+class Quiz : ProgressPrintable {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+
+    override val progressText: String
+        get() = "${answered} of ${total} answered"
+
+    override fun printProgressBar() {
+        repeat(Quiz.answered) { print("▓") }
+        repeat(Quiz.total - Quiz.answered) { print("▒") }
+        println()
+        println(progressText)
+    }
+
+    fun printQuiz() {
+        question1.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+        question2.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+        question3.let {
+            println(it.questionText)
+            println(it.answer)
+            println(it.difficulty)
+        }
+        println()
+    }
+}
+
+interface ProgressPrintable {
+    val progressText: String
+    fun printProgressBar()
+}
+
+/*fun Quiz.StudentProgress.printProgressBar() {
+    repeat(Quiz.answered) { print("▓") }
+    repeat(Quiz.total - Quiz.answered) { print("▒") }
+    println()
+    /*println(Quiz.progressText)*/
+}*/
+
+/*val Quiz.StudentProgress.progressText: String
+    get() = "${answered} of ${total} answered"*/
+
+
+fun main() {
+    Quiz().apply {
+        printQuiz()
+    }
+    /*val quiz = Quiz().apply {
+        printQuiz()
+    }*/
+
+    /*var quiz = Quiz()
+    quiz.printQuiz()*/
+
+    /*Quiz().printProgressBar()*/
+
+    /*println(Quiz.progressText)*/
+
+    /*val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+    println(question1.toString())*/
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
