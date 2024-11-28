e94a37d6d13d2f269725139b2e0869b979c2cf67
ZOUBAIRDouae <zoubairdouae5@gmail.com>
2024-11-28 12:50:21 +0000

    tuto3

--- Module-3/tuto7/app/src/main/java/com/example/tuto71/tuto7.3.kt
+++ Module-3/tuto7/app/src/main/java/com/example/tuto71/tuto7.3.kt
@@ -52,7 +52,48 @@ val cookies = listOf(
     )
 )
 fun main() {
-    cookies.forEach {
-        println("Menu item: $it")
+    /*cookies.forEach {
+        println("Menu item: ${it.name}")
+    }*/
+
+    /*val fullMenu = cookies.map {
+        "${it.name} - $${it.price}"
     }
+
+    println("Full menu:")
+    fullMenu.forEach {
+        println(it)
+    }*/
+
+    /*val softBakedMenu = cookies.filter {
+        it.softBaked
+    }
+*/
+
+    /*val groupedMenu = cookies.groupBy { it.softBaked }
+    val softBakedMenu = groupedMenu[true] ?: listOf()
+    val crunchyMenu = groupedMenu[false] ?: listOf()
+
+    println("Soft cookies:")
+    softBakedMenu.forEach {
+        println("${it.name} - $${it.price}")
+    }
+    println("Crunchy cookies:")
+    crunchyMenu.forEach {
+        println("${it.name} - $${it.price}")
+    }
+
+    val totalPrice = cookies.fold(0.0) {total, cookie ->
+        total + cookie.price
+    }
+    println("Total price : $${totalPrice}")*/
+
+    val alphabeticalMenu = cookies.sortedBy {
+        it.name
+    }
+    println("Alphabetical Menu:")
+    alphabeticalMenu.forEach {
+        println(it.name)
+    }
+
 }
